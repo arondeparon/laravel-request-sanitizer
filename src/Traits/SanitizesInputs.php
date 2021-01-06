b3ead1044f83c845fb02d32da8e90b7a2f3c356d
John Paul Medina <jp@leadtrust.io>
2021-01-05 23:24:05 -0500

    Added a wildcard * sanitizer key - to apply on all inputs

--- src/Traits/SanitizesInputs.php
+++ src/Traits/SanitizesInputs.php
@@ -18,6 +18,27 @@ trait SanitizesInputs
         $this->sanitize();
     }
 
+    public function hasWildcardSanitizer()
+    {
+        return array_key_exists('*', $this->getSanitizers());
+    }
+
+    protected function doSanitize(&$input, $key, $value, $formKey)
+    {
+        if (is_string($key)) {
+            $sanitizer = app()->make($key, $value);
+        } elseif (is_string($value)) {
+            $sanitizer = app()->make($value);
+        } elseif ($value instanceof Sanitizer) {
+            $sanitizer = $value;
+        } else {
+            throw new InvalidArgumentException('Could not resolve sanitizer from given properties');
+        }
+
+        Arr::set($input, $formKey, $sanitizer->sanitize($this->input($formKey, null)));
+        $this->replace($input);
+    }
+
     /**
      * @return mixed
      * @throws \Illuminate\Contracts\Container\BindingResolutionException
@@ -26,26 +47,23 @@ trait SanitizesInputs
     {
         $input = $this->all();
 
+        // Run Wildcards First
+        if ($this->hasWildcardSanitizer()) {
+            $sanitizers = (array) $this->getSanitizers()['*'];
+            foreach ($input as $formKey => $inputValue) {
+                foreach ($sanitizers as $key => $value) {
+                    $this->doSanitize($input, $key, $value, $formKey);
+                }
+            }
+        }
+
         foreach ($this->getSanitizers() as $formKey => $sanitizers) {
             if (!$this->has($formKey)) {
-                // If the request does not have a property for this key, there is no need to sanitize anything.
                 continue;
             }
             $sanitizers = (array) $sanitizers;
             foreach ($sanitizers as $key => $value) {
-                if (is_string($key)) {
-                    $sanitizer = app()->make($key, $value);
-                } elseif (is_string($value)) {
-                    $sanitizer = app()->make($value);
-                } elseif ($value instanceof Sanitizer) {
-                    $sanitizer = $value;
-                } else {
-                    throw new InvalidArgumentException('Could not resolve sanitizer from given properties');
-                }
-
-                Arr::set($input, $formKey, $sanitizer->sanitize($this->input($formKey, null)));
-
-                $this->replace($input);
+                $this->doSanitize($input, $key, $value, $formKey);
             }
         }
 
