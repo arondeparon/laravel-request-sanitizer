0ef2264fac2876f32bdd5292890582b4b2fef275
Anees Khan <30714223+aneeskhan47@users.noreply.github.com>
2021-05-06 08:26:33 +0300

    XssSanitize Test

---
+++ tests/Sanitizers/XssSanitizeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ArondeParon\RequestSanitizer\Tests\Sanitizers;
+
+use ArondeParon\RequestSanitizer\Sanitizers\XssSanitize;
+use ArondeParon\RequestSanitizer\Tests\TestCase;
+
+class TrimTest extends TestCase
+{
+    public function test_trim_sanitizer()
+    {
+        $sanitizer = new XssSanitize();
+        $output = $sanitizer->sanitize("<?php echo 'test'; ?> <script> alert('test') </script>");
+        $this->assertEquals("&lt;?php echo 'test'; ?&gt; &lt;script&gt; alert('test') &lt;/script&gt;", $output);
+    }
